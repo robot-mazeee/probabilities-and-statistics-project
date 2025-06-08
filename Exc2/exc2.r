ba0451561169e47948e53a2ea1a9b7fcec4642bd
robot-mazeee <madalenacmo@gmail.com>
2025-06-08 21:49:41 +0100

    exc 2 corrections

--- Exc2/exc2.r
+++ Exc2/exc2.r
@@ -1,17 +1,16 @@
 library(readxl)
 library(ggplot2)
 
-# Read and filter the data
 data = read_excel("Exc2/wine_prod_EU.xlsx")
 data = subset(data, Category != '-' & `Product Group` != 'Non-Vinified')
-
-# Filter year 2000
 data2000 = subset(data, Year == 2000)
 
 data2000$`Member State`[
   data2000$`Member State` != 'France' & 
   data2000$`Member State` != 'Italy' &
-  data2000$`Member State` != 'Spain'] <- 'Others'
+  data2000$`Member State` != 'Spain' &
+  data2000$`Member State` != 'Germany' &
+  data2000$`Member State` != 'Portugal'] <- 'Others'
 
 ggplot(data2000, aes(x = Category, y = Production, fill = `Member State`)) +
   geom_bar(stat = "identity", position = "dodge") +
@@ -21,4 +20,4 @@ ggplot(data2000, aes(x = Category, y = Production, fill = `Member State`)) +
     y = "Production"
   )
 
-ggsave("plot.png", width = 8, height = 5)
+ggsave("plot.png", width = 5, height = 3)
