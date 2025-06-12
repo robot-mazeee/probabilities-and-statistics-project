aef9d4ca9e152f1f99ae1a44331605f59553f045
robot-mazeee <madalenacmo@gmail.com>
2025-06-12 23:04:38 +0100

    exc 10 corrections, prints and comments

--- Exc10/exc10.r
+++ Exc10/exc10.r
@@ -5,10 +5,10 @@ k = 6
 
 # Randomly select n-sized sample with no replacement
 full_sample = scan("Exc10/sample.txt", sep = ",")
-subsample = sample(full_sample, size = 160, replace = FALSE)
+subsample = sample(full_sample, size = n, replace = FALSE)
 
 # Divide sample in 6 equiprobable classes
-probs = seq(1/6, 5/6, by = 1/6)
+probs = seq(1/k, (k-1)/k, by = 1/k)
 # Inverted Rayleigh
 cutoffs = sigma0 * sqrt(-2 * log(1 - probs))
 breaks = c(0, cutoffs, Inf)
@@ -21,10 +21,26 @@ frequencies <- table(subsample_classes)
 print(frequencies)
 
 # Calculate p-value
-expected_freq <- rep(160 / 6, 6)
-chi_test <- chisq.test(x = as.numeric(frequencies), p = rep(1/6, 6))
+expected_freq <- rep(n / k, k)
+chi_test <- chisq.test(x = as.numeric(frequencies), p = rep(1/k, k))
 p_value = chi_test$p.value
 cat("p-value of test:", p_value, "\n")
+
+if (p_value < 0.01) {
+    print("Reject H0 for insignificance level of 1%.")
+} else {
+    print("Don't reject H0 for insignificance level of 1%.")
+} 
+if (p_value < 0.05) {
+    print("Reject H0 for insignificance level of 5%.")
+} else {
+    print("Don't reject H0 for insignificance level of 5%.")
+} 
+if (p_value < 0.1) {
+    print("Reject H0 for insignificance level of 10%.")
+} else {
+    print("Don't reject H0 for insignificance level of 10%.")
+}
 
 #  0.009841374 < 0.01
 #  0.009841374 < 0.05
