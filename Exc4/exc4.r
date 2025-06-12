c19e6128b35d1b41e0f569754d30bd174ab24e66
robot-mazeee <madalenacmo@gmail.com>
2025-06-12 22:47:27 +0100

    exc 4 comments and prints

--- Exc4/exc4.r
+++ Exc4/exc4.r
@@ -4,6 +4,7 @@ k = 11
 
 # Calculate X expected value with Gamma
 expected_value = λ * gamma(1 + 1/k)
+cat("Expected value:", expected_value, '\n')
 
 set.seed(1819)
 
@@ -15,6 +16,8 @@ sample = rweibull(n, shape = k, scale = λ)
 # Calculate Monte Carlo expected value
 mean_monte_carlo = mean(sample)
 
+cat("Monte Carlo value:", mean_monte_carlo, '\n')
+
 dif = round(abs(expected_value - mean_monte_carlo), 4)
 
-print(dif)
+cat("Difference between expected and Monte Carlo values:", dif, '\n')
