86835eedb3596ff122ab621ec096e2997abb9287
robot-mazeee <madalenacmo@gmail.com>
2025-06-15 00:11:00 +0100

    last corrections and all exercises

--- AllExcs.r
+++ AllExcs.r
@@ -6,6 +6,7 @@ k = 11
 
 # Calculate X expected value with Gamma
 expected_value = λ * gamma(1 + 1/k)
+cat("Expected value:", expected_value, '\n')
 
 set.seed(1819)
 
@@ -17,9 +18,11 @@ sample = rweibull(n, shape = k, scale = λ)
 # Calculate Monte Carlo expected value
 mean_monte_carlo = mean(sample)
 
+cat("Monte Carlo value:", mean_monte_carlo, '\n')
+
 dif = round(abs(expected_value - mean_monte_carlo), 4)
 
-print(dif)
+cat("Difference between expected and Monte Carlo values:", dif, '\n')
 
 # --------------------------------- exc 5 ---------------------------------
 
@@ -116,10 +119,12 @@ score_function = function(alpha) {
 
 # Maximum likelihood estimates
 estimated_alpha = uniroot(score_function, interval = c(0.001, 169.8))$root
+cat("Maximum likelihood alpha estimate:", estimated_alpha, '\n')
 estimated_lambda = estimated_alpha / xi_mean
+cat("Maximum likelihood lambda estimate:", estimated_lambda, '\n')
 
 mode = round((estimated_alpha - 1) / estimated_lambda, 2)
-print(mode)
+cat("Modal length:", mode, '\n')
 
 # --------------------------------- exc 8 ---------------------------------
 
@@ -177,11 +182,12 @@ accept_H0 = T0 <= crit_value
 
 # Beta estimate
 beta_hat = mean(accept_H0)
+
 # Beta theoretical value
-beta = pgamma(crit_value, shape = 2 * n, rate = mu0 / mu1)
+beta = pchisq(crit_value*(mu0/mu1), df = 2*n)
 
 quotient = round(beta_hat / beta, 4)
-print(quotient)
+cat("Quotient between estimate and theoretical beta:", quotient, '\n')
 
 # --------------------------------- exc 10 --------------------------------
 
@@ -192,10 +198,10 @@ k = 6
 
 # Randomly select n-sized sample with no replacement
 full_sample = scan("Exc10/sample.txt", sep = ",")
-subsample = sample(full_sample, size = 160, replace = FALSE)
+subsample = sample(full_sample, size = n, replace = FALSE)
 
 # Divide sample in 6 equiprobable classes
-probs = seq(1/6, 5/6, by = 1/6)
+probs = seq(1/k, (k-1)/k, by = 1/k)
 # Inverted Rayleigh
 cutoffs = sigma0 * sqrt(-2 * log(1 - probs))
 breaks = c(0, cutoffs, Inf)
@@ -208,10 +214,26 @@ frequencies <- table(subsample_classes)
 print(frequencies)
 
 # Calculate p-value
-expected_freq <- rep(160 / 6, 6)
-chi_test <- chisq.test(x = as.numeric(frequencies), p = rep(1/6, 6))
+expected_freq <- rep(n / k, k)
+chi_test <- chisq.test(x = as.numeric(frequencies), p = rep(1/k, k))
 p_value = chi_test$p.value
 cat("p-value of test:", p_value, "\n")
+
+if (p_value < 0.01) {
+    print("Reject H0 for insignificance level of 1%.")
+} else {
+    print("Don't reject H0 for insignificance level of 1%.")
+} 
+if (p_value < 0.05) {
+    print("Reject H0 for insignificance level of 5%.")
+} else {
+    print("Don't reject H0 for insignificance level of 5%.")
+} 
+if (p_value < 0.1) {
+    print("Reject H0 for insignificance level of 10%.")
+} else {
+    print("Don't reject H0 for insignificance level of 10%.")
+}
 
 #  0.009841374 < 0.01
 #  0.009841374 < 0.05
